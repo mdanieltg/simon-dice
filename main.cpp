376f1fd0fcc2f5e2d95794d3cd1525a3d950cae9
Daniel Medrano Huerta <daniel@quicktortoise.com>
2019-11-07 18:50:44 -0600

    Programa finalizado

--- main.cpp
+++ main.cpp
@@ -5,49 +5,28 @@
 
 using namespace std;
 
-void wait(int);
 char rand_num();
+void game(int);
 
 int main()
 {
-	int max_phases = 10,
-		current_phase = 0;
-	char keys[max_phases];
+	int levels;
 
-	while (current_phase <= max_phases)
-	{
-		keys[current_phase++] = rand_num();
+	cout << "Simón dice" << endl;
+	cout << "Cantidad de niveles (0 para infinito): ";
+	cin >> levels;
 
-		// Mostrar secuencia en pantalla, caracter por caracter
-		for (int i = 0; i < current_phase; i++)
-		{
-			cout << "\033[2J\033[1;1H";
-			cout << "  " << keys[i] << flush;
+	cout << "\033[2J\033[1;1H";
 
-			this_thread::sleep_for(chrono::seconds(1));
-		}
-		cout << "\033[2J\033[1;1H" << "    " << flush;
-
-		// Leer secuencia de la entrada estándar y compararla
-		for (int i = 0; i < current_phase; i++)
-		{
-			char input;
-			do
-			{
-				cin.get(input);
-			} while (input == 13);
-
-			if (input != keys[i])
-				return 1;
-		}
-	}
-
-	cout << "Ganaste!" << endl;
+	if (levels > 0)
+		game(levels);
+	else
+		game(32000);
 
 	return 0;
 }
 
-// Generar número aleatorio entre 97 y 122
+// Generar caracter (ASCII) aleatorio entre 97 y 122
 char rand_num()
 {
 	int number;
@@ -61,4 +40,53 @@ char rand_num()
 	return (char)number;
 }
 
+// Juego
+void game(int max_phases)
+{
+	char keys[max_phases];
+	int current_phase = 0;
+
+	while (current_phase < max_phases)
+	{
+		// Asignar letra aleatoria
+		keys[current_phase++] = rand_num();
+
+		// Mostrar secuencia en pantalla, caracter por caracter
+		for (int i = 0; i < current_phase; i++)
+		{
+			cout << "\033[2J\033[1;1H";
+			cout << "  " << keys[i] << flush;
+
+			this_thread::sleep_for(chrono::seconds(1));
+		}
+		cout << "\033[2J\033[1;1H" << "Tu respuesta: " << flush;
+
+		// Leer secuencia de la entrada estándar y compararla
+		for (int i = 0; i < current_phase; i++)
+		{
+			char input;
+			do
+			{
+				cin.get(input);
+			} while (input == 10);
+
+			if (input != keys[i])
+			{
+				cout << "Nivel " << current_phase << endl;
+				cout << "La secuencia era ";
+
+				for (int j = 0; j < current_phase; j++)
+					cout << keys[j];
+
+				cout << endl;
+
+				return;
+			}
+		}
+	}
+
+	cout << "¡Ganaste!" << endl;
+
+	return;
+}
 // vim: set number:
